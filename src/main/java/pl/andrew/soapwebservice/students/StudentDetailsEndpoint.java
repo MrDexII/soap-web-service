3e76e28cd69d737ae9761e654ea02364d46c816b
MrDexII <free.andrew.a@gmail.com>
2020-12-09 20:07:24 +0100

    db config

---
+++ src/main/java/pl/andrew/soapwebservice/students/StudentDetailsEndpoint.java
@@ -0,0 +1,24 @@
+package pl.andrew.soapwebservice.student;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class StudentDetailsEndpoint {
+    @PayloadRoot(namespace = "http://andrew.pl/students", localPart = "GetStudentDetailsRequest")
+    @ResponsePayload
+    public GetStudentDetailsResponse processCourseDetailsRequest(@RequestPayload GetStudentDetailsRequest request){
+        GetStudentDetailsResponse response = new GetStudentDetailsResponse();
+
+        StudentDetails studentDetails = new StudentDetails();
+        studentDetails.setId(request.getId());
+        studentDetails.setName("Andrzej");
+        studentDetails.setPassportNumber("E1234567");
+
+        response.setStudentDetails(studentDetails);
+
+        return response;
+    }
+}
---
+++ src/main/java/pl/andrew/soapwebservice/students/WebServiceConfig.java
@@ -0,0 +1,40 @@
+package pl.andrew.soapwebservice.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(context);
+        messageDispatcherServlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(messageDispatcherServlet, "/ws/*");
+    }
+
+    @Bean(name = "students")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("StudentPort");
+        definition.setLocationUri("/ws");
+        definition.setTargetNamespace("http://andrew.pl/soapwebservice/students");
+        definition.setSchema(studentSchema);
+        return definition;
+    }
+
+    @Bean
+    public XsdSchema studentSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("student.xsd"));
+    }
+}
